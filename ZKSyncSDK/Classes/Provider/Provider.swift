16f6b0219824364c3eb15e445a76c13d4020f0f1
Eugene Belyakov <eugen.belyakov@gmail.com>
2021-01-13 19:18:20 +0100

    Rearranged methods

--- ZKSyncSDK/Classes/Provider/Provider.swift
+++ ZKSyncSDK/Classes/Provider/Provider.swift
@@ -12,14 +12,13 @@ public protocol Provider {
     func accountState(address: String,
                       completion: @escaping (ZKSyncResult<AccountState>) -> Void)
     
-    func contractAddress(completion: @escaping (ZKSyncResult<ContractAddress>) -> Void)
-
-    func tokenPrice(token: Token, completion: @escaping (ZKSyncResult<Decimal>) -> Void)
-    
-    func tokens(completion: @escaping (ZKSyncResult<Tokens>) -> Void)
-    
     func transactionFee(request: TransactionFeeRequest, completion: @escaping (ZKSyncResult<TransactionFeeDetails>) -> Void)
     
     func transactionFee(request: TransactionFeeBatchRequest, completion: @escaping (ZKSyncResult<TransactionFeeDetails>) -> Void)
 
+    func tokens(completion: @escaping (ZKSyncResult<Tokens>) -> Void)
+
+    func tokenPrice(token: Token, completion: @escaping (ZKSyncResult<Decimal>) -> Void)
+    
+    func contractAddress(completion: @escaping (ZKSyncResult<ContractAddress>) -> Void)
 }
