93d4ef0c4255d1c8263d1351056add0af8686e8e
Eugene Belyakov <eugen.belyakov@gmail.com>
2021-01-16 18:13:55 +0100

    Moved transaction fee to separate file

--- ZKSyncSDK/Classes/Wallet/DefaultWallet.swift
+++ ZKSyncSDK/Classes/Wallet/DefaultWallet.swift
@@ -48,30 +48,6 @@ public class DefaultWallet: Wallet {
         self.provider.accountState(address: self.ethSigner.address, queue: queue, completion: completion)
     }
 
-    public func getTransactionFee(for transactionType:TransactionType,
-                                  tokenIdentifier: String,
-                                  completion: @escaping ZKSyncCompletion<TransactionFeeDetails>) {
-        self.getTransactionFee(for: transactionType,
-                               address: ethSigner.address,
-                               tokenIdentifier: tokenIdentifier,
-                               completion: completion)
-    }
-    
-    public func getTransactionFee(for transactionType:TransactionType,
-                                  address: String,
-                                  tokenIdentifier: String,
-                                  completion: @escaping ZKSyncCompletion<TransactionFeeDetails>) {
-        let request = TransactionFeeRequest(transactionType: transactionType,
-                                            address: address,
-                                            tokenIdentifier: tokenIdentifier)
-        self.provider.transactionFee(request: request, completion: completion)
-    }
-    
-    public func getTransactionFee(for batchRequest: TransactionFeeBatchRequest,
-                           completion: @escaping ZKSyncCompletion<TransactionFeeDetails>) {
-        self.provider.transactionFee(request: batchRequest, completion: completion)
-    }
-
     public var isSigningKeySet: Bool {
         pubKeyHash == zkSigner.publicKeyHash
     }
