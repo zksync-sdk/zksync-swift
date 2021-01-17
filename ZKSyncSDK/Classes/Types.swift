5318a3e1c4bd43dd2fda7a41078b8615da13f320
Eugene Belyakov <eugen.belyakov@gmail.com>
2021-01-17 11:15:22 +0100

    Cleanup

--- ZKSyncSDK/Classes/Types.swift
+++ ZKSyncSDK/Classes/Types.swift
@@ -9,17 +9,3 @@ import Foundation
 import BigInt
 
 public typealias ZKSyncResult<T> = Result<T, Error>
-
-enum VerificationMethod {
-    case ECDSA, ERC1271
-}
-
-struct EthSignerType {
-    var verificationMethod: VerificationMethod
-    var signedMsgPrefixed: Bool
-}
-
-enum TxEhSignature {
-    case Ethereum(signature: String)
-    case EIP1271(signature: String)
-}
