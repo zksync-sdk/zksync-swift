14bf92303cc059416cfe7ed0b775f38c540e5c2c
Eugene Belyakov <eugen.belyakov@gmail.com>
2021-03-20 09:23:58 +0100

    Added time range

---
+++ ZKSyncSDK/Classes/Model/TimeRange.swift
@@ -0,0 +1,13 @@
+//
+//  TimeRange.swift
+//  ZKSync
+//
+//  Created by Eugene Belyakov on 20/03/2021.
+//
+
+import Foundation
+
+public struct TimeRange: Decodable {
+    public var validFrom: UInt64
+    public var validUntil: UInt64
+}
