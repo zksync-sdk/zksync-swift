c22a8d822d489b704c207d7bee98ff9e4bde9271
Eugene Belyakov <eugen.belyakov@gmail.com>
2021-05-23 13:53:33 +0200

    Improved unit testing of Withdraw

--- Example/Tests/DefaultWalletTests.swift
+++ Example/Tests/DefaultWalletTests.swift
@@ -405,3 +405,34 @@ extension Transfer: Equatable {
         return tx
     }
 }
+
+extension Withdraw: Equatable {
+    
+    public static func == (lhs: Withdraw, rhs: Withdraw) -> Bool {
+        return lhs.accountId == rhs.accountId &&
+            lhs.from == rhs.from &&
+            lhs.to == rhs.to &&
+            lhs.token == rhs.token &&
+            lhs.amount == rhs.amount &&
+            lhs.fee == rhs.fee &&
+            lhs.nonce == rhs.nonce &&
+            lhs.timeRange.validFrom == rhs.timeRange.validFrom &&
+            lhs.timeRange.validUntil == rhs.timeRange.validUntil &&
+            lhs.signature?.pubKey == rhs.signature?.pubKey &&
+            lhs.signature?.signature == rhs.signature?.signature
+    }
+
+    static var defaultTX: Withdraw {
+        let tx = Withdraw(accountId: 44,
+                          from: "0xede35562d3555e61120a151b3c8e8e91d83a378a",
+                          to: "0x19aa2ed8712072e918632259780e587698ef58df",
+                          token: 0,
+                          amount: 1000000000000,
+                          fee: "1000000",
+                          nonce: 12,
+                          timeRange: .max)
+        tx.signature = Signature(pubKey: "40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490",
+                                 signature: "3e2866bb00f892170cc3592d48aec7eb4afba75bdd0a530780fa1dcbdf857d07d75deb774142a93e3d1ca3be29e614e50892b95702b6461f86ddf78b9ab11a01")
+        return tx
+    }
+}
