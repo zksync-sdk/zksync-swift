c9fcf5fc2dd64f6548062da26030e2b12b4b2754
Eugene Belyakov <eugen.belyakov@gmail.com>
2021-05-23 14:05:06 +0200

    Improved unit testing of WithdrawNFT

--- Example/Tests/DefaultWalletTests.swift
+++ Example/Tests/DefaultWalletTests.swift
@@ -171,6 +171,9 @@ class DefaultWalletTests: XCTestCase {
         }
         waitForExpectations(timeout: 5, handler: nil)
         XCTAssertEqual(try result?.get(), "success:hash")
+        let receivedTX = provider.received as? WithdrawNFT
+        XCTAssertNotNil(receivedTX)
+        XCTAssertEqual(receivedTX, WithdrawNFT.defaultTX)
     }
 
     func testGetState() throws {
@@ -470,3 +473,35 @@ extension MintNFT: Equatable {
         return tx
     }
 }
+
+extension WithdrawNFT: Equatable {
+    
+    public static func == (lhs: WithdrawNFT, rhs: WithdrawNFT) -> Bool {
+        return lhs.accountId == rhs.accountId &&
+            lhs.from == rhs.from &&
+            lhs.to == rhs.to &&
+            lhs.token == rhs.token &&
+            lhs.feeToken == rhs.feeToken &&
+            lhs.fee == rhs.fee &&
+            lhs.nonce == rhs.nonce &&
+            lhs.timeRange.validFrom == rhs.timeRange.validFrom &&
+            lhs.timeRange.validUntil == rhs.timeRange.validUntil &&
+            lhs.signature?.pubKey == rhs.signature?.pubKey &&
+            lhs.signature?.signature == rhs.signature?.signature
+    }
+
+    static var defaultTX: WithdrawNFT {
+        let tx = WithdrawNFT(accountId: 44,
+                             from: "0xede35562d3555e61120a151b3c8e8e91d83a378a",
+                             to: "0x19aa2ed8712072e918632259780e587698ef58df",
+                             token: 100000,
+                             feeToken: 0,
+                             fee: "1000000",
+                             nonce: 12,
+                             timeRange: .max)
+        tx.signature = Signature(pubKey: "40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490",
+                                 signature: "9d94324425f23d09bf76df52e520e8da4561718057eb29fe6d760945be986b8e3a1955d9c02cf415558f533b7d9573564798db9586cc5ba1fdc44f711e455e03")
+        return tx
+    }
+}
+
