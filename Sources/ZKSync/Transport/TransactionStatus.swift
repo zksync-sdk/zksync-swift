232e6f5cc9506acbb55c393d04a8486b87824acb
Maxim Makhun <mmakhun@vareger.com>
2021-07-18 17:48:33 -0700

    Add documentation for `TransactionStatus`.

--- Sources/ZKSync/Transport/TransactionStatus.swift
+++ Sources/ZKSync/Transport/TransactionStatus.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// Enum, which is used by `PollingTransactionReceiptProcessor` to verify in what transaction status
+/// it should be waiting for.
 public enum TransactionStatus {
     case sent
     case commited
