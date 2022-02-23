e01e5792bbaed4c1c42af65ba077af76830f5389
Michael Brown <me@michael-brown.net>
2022-02-23 09:29:49 +0000

    Updated ZKSyncCrypto.xcframework with version that supports arm64 simulators on M1 Macs.

---
+++ Dependencies/ZKSyncCrypto.xcframework/ios-arm64_x86_64-simulator/ZKSyncCrypto.framework/Headers/ZKSyncCrypto.h
@@ -0,0 +1,18 @@
+//
+//  ZKSyncCrypto.h
+//  ZKSyncCrypto
+//
+//  Made with ❤️ by Matter Labs on 10/23/20
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZKSyncCrypto.
+FOUNDATION_EXPORT double ZKSyncCryptoVersionNumber;
+
+//! Project version string for ZKSyncCrypto.
+FOUNDATION_EXPORT const unsigned char ZKSyncCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZKSyncCrypto/PublicHeader.h>
+
+#import "zks_crypto.h"
